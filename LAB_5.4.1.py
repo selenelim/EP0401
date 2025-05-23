89e9944954a1fdf842f8364da1753b4481ec9e3e
selenelim <161735800+selenelim@users.noreply.github.com>
2025-05-23 16:02:17 +0800

    Update LAB_5.4.1.py

--- LAB_5.4.1.py
+++ LAB_5.4.1.py
@@ -1,7 +1,7 @@
 import requests
 
-TOKEN = "7716903147:AAHH67-STADFwfLwSdjs0Fbcaj1QWWhgUmE"
-chat_id = "1485493857"
+TOKEN = "put token here"
+chat_id = "put chat id here"
 message = "Sending a Telegram message from Python code..."
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
